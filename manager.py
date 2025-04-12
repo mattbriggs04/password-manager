ca67a026c8d19b419d07331dc09b6794089850a5
Matt Briggs <matthew.briggs04@gmail.com>
2025-04-11 23:49:23 -0400

    general framework and brainstorming added

--- manager.py
+++ manager.py
@@ -1,21 +1,50 @@
-import os
 import subprocess
 import json
 import getpass # gets passwords hidden
 
 # Could even have a file that is like a vimrc where they choose options, such as if password is optional
+settings = {}
+
+def get_settings(filepath="settings.json"):
+    with open(filepath, "r") as f:
+        settings = json.load(f.read())
 
 def get_entry():
-    site = input("\nEnter the site name: ")
+    info = {}
+    site = input("\nSite name: ")
+    siteurl = input("Site url (press enter to leave blank): ")
+    username = input("Username")
+
+    # if settings['password_hidden']
     password = getpass.getpass("Password (input hidden): ")
+    notes = input("")
 
-def create_new_file(filename="passwords.json"):
+    info = {
+        "siteurl": siteurl,
+        "username": username,
+        "password": password,
+
+    }
+    return site, info
+
+def create_new_file():
+    filename = input("Enter a file name (press enter to name file \"passwords.json\"): ")
+    if filename == "":
+        filename = "passwords.json"
+
     entries = {}
-
-    while True:
+    entry_cont = input("Would you like to add one or more entries [y/n]? ")
+    while entry_cont == 'y':
         site, entry = get_entry()
+        entry_cont = input("Would you like to add another entry [y/n]? ")
+
     with open(filename, "w") as f:
+        print("Populating file", filename)
         json.dump(entries)
+
+def gpg_encrypt_file(plain_filepath, encrypted_filepath):
+    cipher_algo = "AES" # until I add a settings parser
+    subprocess.run(["gpg", "--yes", "--cipher-algo", cipher_algo, "-o", ])
 
 def print_help():
     print("Required package") # TODO: dependency list (secure-remove, gpg)
@@ -27,12 +56,14 @@ def menu():
     print("1. Create a new file")
     print("2. Add an entry to an existing file")
 
+    # print("Change settings")
+
     user_option = int(input("Enter an option: "))
     match user_option:
         case 0:
             print_help()
         case 1:
-            create_new()
+            create_new_file()
         case 2:
             pass
         case _:
