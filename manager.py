8626c0e86a1f2a0ad0e23158f53bde9aeb1ecdc6
Matt Briggs <matthew.briggs04@gmail.com>
2025-04-12 00:00:28 -0400

    outline and TODO comments added, more logic for entry adding

--- manager.py
+++ manager.py
@@ -17,13 +17,13 @@ def get_entry():
 
     # if settings['password_hidden']
     password = getpass.getpass("Password (input hidden): ")
-    notes = input("")
+    notes = input("Additional Notes: ") # maybe add multi-line notes allowed
 
     info = {
         "siteurl": siteurl,
         "username": username,
         "password": password,
-
+        "notes": notes,
     }
     return site, info
 
@@ -34,8 +34,14 @@ def create_new_file():
 
     entries = {}
     entry_cont = input("Would you like to add one or more entries [y/n]? ")
+    sites = set()
     while entry_cont == 'y':
         site, entry = get_entry()
+        if site in sites:
+            print("Site already added - would you like to overwrite?")
+            # TODO: if (y) overwrite
+        else:
+            sites.add(site)
         entry_cont = input("Would you like to add another entry [y/n]? ")
 
     with open(filename, "w") as f:
@@ -44,14 +50,14 @@ def create_new_file():
 
 def gpg_encrypt_file(plain_filepath, encrypted_filepath):
     cipher_algo = "AES" # until I add a settings parser
-    subprocess.run(["gpg", "--yes", "--cipher-algo", cipher_algo, "-o", ])
+    subprocess.run(["gpg", "--yes", "--cipher-algo", cipher_algo, "-o", ]) # TODO: this command
 
 def print_help():
     print("Required package") # TODO: dependency list (secure-remove, gpg)
     print("Current settings: ") # print settings
 
 def menu():
-    print("\n----- Password Manager Menu -----")
+    print("\n------- Password Manager Menu -------")
     print("0. Help, Settings, and Dependencies")
     print("1. Create a new file")
     print("2. Add an entry to an existing file")
@@ -71,6 +77,7 @@ def menu():
             menu()
 
 if __name__ == "__main__":
+    # make a cooler load in using ASCII art for the letters
     print("/ / / / / / / / / / / / / / / / / / /")
     print("    GPG      PASSWORD      MANAGER   ")
     print("/ / / / / / / / / / / / / / / / / / /")
