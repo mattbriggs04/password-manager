54d3a73b1443801687005d4a6e4812bf3ce99cc8
Matt Briggs <matthew.briggs04@gmail.com>
2025-04-19 02:08:20 -0400

    working json to password file

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 import getpass # gets passwords hidden
-
+import time
 # Could even have a file that is like a vimrc where they choose options, such as if password is optional
 settings = {}
 
@@ -14,7 +14,7 @@ def get_entry() -> tuple[str, dict[str, str]]:
     info = {}
     site = input("\nSite name: ")
     siteurl = input("Site url (press enter to leave blank): ")
-    username = input("Username")
+    username = input("Username: ")
 
     # if settings['password_hidden']
     password = getpass.getpass("Password (input hidden): ")
@@ -41,35 +41,57 @@ def create_new_file():
     if filename == "":
         filename = "passwords.json"
 
+    json_dict = {}
     entries = {}
-    entry_cont = input("Would you like to add one or more entries [y/n]? ")
+    entry_cont_rsp = input("Would you like to add one or more entries [y/n]? ")
     sites = set()
-    while entry_cont == 'y':
+    while entry_cont_rsp.lower() == 'y':
         site, entry = get_entry()
         if site in sites:
-            print("Site already added - would you like to overwrite?")
-            # TODO: if (y) overwrite
+            exists_rsp = input("Site already added - would you like to overwrite [y/n]? ")
+            if exists_rsp.lower() == 'y':
+                json_dict[site] = entry
         else:
             sites.add(site)
-        entry_cont = input("Would you like to add another entry [y/n]? ")
+            json_dict[site] = entry
+        entry_cont_rsp = input("\nWould you like to add another entry [y/n]? ")
 
-    with open(filename, "w") as f:
+    with open(filename, "w") as json_file:
         print("Populating file", filename)
-        json.dump(entries)
+        json.dump(obj=json_dict, fp=json_file, indent=4)
 
-def gpg_encrypt_file(plain_filepath, encrypted_filepath):
-    cipher_algo = "AES" # until I add a settings parser
-    subprocess.run(["gpg", "--yes", "--cipher-algo", cipher_algo, "-o", ]) # TODO: this command
+def gpg_encrypt_file(filepath, passphrase):
+    command = [
+        "gpg",
+        "--batch",
+        "--yes",
+        "--quiet",
+        "--pinentry-mode", "loopback",
+        "--passphrase", passphrase,
+        "--cipher-algo", settings["encryptionAlgo"],
+        "-c", filepath
+
+    ]
+    subprocess.run(command, ) # TODO: this command
+
+    print("File successfully encrypted, would you like to securely remove the unencrypted file [y/n]?")
+
+def gpg_decrypt_file(filepath, passphrase):
+    pass
+
+def gpg_site_info(filepath, passphrase, site):
+    pass # searches the file for the site, if so, returns username and password to user
 
 def print_help():
     print("Required package") # TODO: dependency list (secure-remove, gpg)
     print("Current settings:", settings) # print settings
 
-def menu():
+def menu() -> bool:
     print("\n------- Password Manager Menu -------")
     print("0. Help, Settings, and Dependencies")
-    print("1. Create a new file")
+    print("1. Create a new password file")
     print("2. Add an entry to an existing file")
+
 
     # print("Change settings")
 
@@ -82,8 +104,10 @@ def menu():
         case 2:
             pass
         case _:
-            print("Invalid option")
-            menu()
+            print("Quitting password manager")
+            return False
+        
+    return True
 
 if __name__ == "__main__":
     # make a cooler load in using ASCII art for the letters
@@ -98,4 +122,6 @@ _/____/_/__/_/_______/_/___/_/__/_/___
           PASSWORD MANAGER
 """)
     load_settings()
-    menu()
+    while menu():
+        time.sleep(2)
+        pass
